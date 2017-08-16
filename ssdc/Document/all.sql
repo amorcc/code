46813cdd5af2186dddbee1756affb44567cef12a
amorcc <31053749+amorcc@users.noreply.github.com>
2017-08-16 17:27:39 +0800

    init2

--- ssdc/Document/all.sql
+++ ssdc/Document/all.sql
@@ -1,28 +1,3 @@
-DELIMITER $$
-
-USE `ssdc`$$
-
-DROP PROCEDURE IF EXISTS `ResultAll`$$
-
-CREATE DEFINER=`root`@`%` PROCEDURE `ResultAll`(iPageIndex INT, iPageSize INT)
-BEGIN
-	SET @startRow = (iPageIndex-1)* iPageSize;  
-	SET @pageSize = iPageSize;  
-	SET @strsql = CONCAT(  
-		'SELECT ID,IdNumber AS ''身份证号'',table1count,table2count,table3count,table4count,table5count,table6count,table7count,table8count FROM contrastresult',
-		' ORDER BY ID',	
-		' limit ',
-		@startRow,
-		',',
-		@pageSize	
-	);  
-    
-	PREPARE strsql FROM @strsql;#定义预处理语句   
-	EXECUTE strsql;             #执行预处理语句   
-	DEALLOCATE PREPARE strsql;  #删除定义
-	
-    
-	SELECT COUNT(0) FROM contrastresult;
-    END$$
-
-DELIMITER ;
+version https://git-lfs.github.com/spec/v1
+oid sha256:2226062710a5c5e3bb5ba0ac742aa1943742fa59fc38bc4be852163c9184dbbf
+size 710
